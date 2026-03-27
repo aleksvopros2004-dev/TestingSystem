601422417ebc1e2de1214b4d1bd03f1b42cbad31
alexander chegodaev <chegodaev_2004@mail.ru>
2026-03-27 11:46:19 +0400

    изменение лемматизации

--- src/TestingSystem.Core/Interfaces/ILemmatizationService.cs
+++ src/TestingSystem.Core/Interfaces/ILemmatizationService.cs
@@ -6,5 +6,9 @@ namespace TestingSystem.Core.Interfaces
     {
         List<string> LemmatizeText(string text);
         List<WordFrequency> GetWordFrequencies(IEnumerable<string> texts, int topCount = 20);
+        string GetDetailedInfo(string word);
+        bool IsStopWord(string word);
+        void AddStopWord(string word);
+        void AddStopWords(IEnumerable<string> words);
     }
 }
